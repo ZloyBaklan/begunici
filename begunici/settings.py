f04a5e6eb1889265b25af98bbac5bede6cdedec4
Tim <timastan2007@gmail.com>
2026-01-31 02:13:49 +0300

    Update settings.py

--- begunici/settings.py
+++ begunici/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-yt8beqip0qjts_3*66)!@16wav5*e#zo=$7yv+hk@__6)5473_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
